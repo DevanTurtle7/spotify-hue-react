3bdc343b1754a6284d8af8e56557567da063e6da
Devan <devankav7@gmail.com>
2021-01-02 21:50:08 -0500

    added ip textbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class ConnectPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {input: ''}
+    this.clickHandler = this.clickHandler.bind(this)
+    this.changeHandler = this.changeHandler.bind(this)
+    this.val = ''
+  }
+
+  changeHandler(event) {
+    this.setState({input: event.target.value})
+  }
+
+  clickHandler() {
+    console.log(this.state.input)
+  }
+
+
   render() {
-    return <h1>Hello</h1>
+    return <div id='ConnectPage'>
+      <h2>Enter Bridge IP Address</h2>
+      <input type='number' placeholder='000.000.000.00' onChange={this.changeHandler}/><br></br>
+      <button onClick={this.clickHandler}>Connect</button>
+  </div>
   }
 }
 
