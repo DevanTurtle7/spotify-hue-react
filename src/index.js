8775d73e15cdba0ca2044210218482490c1fb77c
Devan <devankav7@gmail.com>
2021-01-02 21:12:00 -0500

    stubbed connect page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App status='no-ip'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
